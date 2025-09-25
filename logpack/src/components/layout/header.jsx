594194614f5efc21c098dc5ec59d1cc53faf0213
Sammy-front <sajuan1020@gmail.com>
2025-09-25 11:23:27 -0300

    Design

--- logpack/src/components/layout/header.jsx
+++ logpack/src/components/layout/header.jsx
@@ -44,16 +44,20 @@ export default function Header() {
 
                     {/* Logo */}
                     <div className="flex items-center lg:pl-10 lg:pr-30">
-                        <Avatar className={"w-23 h-17 rounded-none"}>
+                        <Avatar className={"w-12 h-11 lg:w-23 lg:h-17 rounded-none"}>
                             <AvatarImage src="./logo/log1.png" />
                             <AvatarFallback>L</AvatarFallback>
                         </Avatar>
                         {/* px-2 apos isso é o Gradient */}
-                        <h2 className="font-bold text-2xl px-2 bg-gradient-to-r from-[#68482f]  to-[#cf966a] bg-clip-text text-transparent 
+                        <h2 className="font-bold text-xl px-2 bg-gradient-to-r from-[#68482f] to-[#cf966a] bg-clip-text text-transparent 
                         hover:from-[#8f5f3b] hover:to-[#e0a273]" >
                             LogPack
                         </h2>
                     </div>
+
+
+
+
 
                     {/* NavBar */}
                     <NavigationMenu viewport={false} className="hidden lg:block">
@@ -111,7 +115,7 @@ export default function Header() {
                             <DrawerContent className="h-full right-0 mt-0 w-64 fixed bottom-0 rounded-none">
 
                                 <DrawerHeader className="text-left">
-                                    <DrawerTitle>Zenith Navegação</DrawerTitle>
+                                    <DrawerTitle>LogPack Navegação</DrawerTitle>
                                     <DrawerDescription>Explore nossas opções</DrawerDescription>
                                 </DrawerHeader>
 
@@ -119,42 +123,32 @@ export default function Header() {
                                     <Accordion type="single" collapsible className="w-full">
                                         {/* item 1 */}
                                         <AccordionItem value="item-1">
-                                            <AccordionTrigger>Home</AccordionTrigger>
                                             <AccordionContent>
-                                                <a href="/" className="px-4 py-2 block text-sm hover:bg-accent hover:text-accent-foreground rounded-sm">
-                                                    Início
+                                                <a href="/" className="py-2 block text-sm hover:bg-accent hover:text-accent-foreground rounded-sm">
+                                                    Home
                                                 </a>
                                             </AccordionContent>
                                         </AccordionItem>
 
                                         {/* item 2 */}
                                         <AccordionItem value="item-2">
-                                            <AccordionTrigger>Produtos</AccordionTrigger>
-                                            <AccordionContent>
-                                                <a href="#" className="px-4 py-2 block text-sm hover:bg-accent hover:text-accent-foreground rounded-sm">Celulares</a>
-                                                <a href="#" className="px-4 py-2 block text-sm hover:bg-accent hover:text-accent-foreground rounded-sm">Notebooks</a>
-                                                <a href="#" className="px-4 py-2 block text-sm hover:bg-accent hover:text-accent-foreground rounded-sm">Acessórios</a>
-                                            </AccordionContent>
-                                        </AccordionItem>
-
-                                        {/* item 3 */}
-                                        <AccordionItem value="item-3">
                                             <AccordionTrigger>Serviços</AccordionTrigger>
                                             <AccordionContent>
                                                 <a href="/servicos" className="px-4 py-2 block text-sm hover:bg-accent hover:text-accent-foreground rounded-sm">Assistência</a>
                                             </AccordionContent>
                                         </AccordionItem>
 
-                                        {/* item 4 */}
-                                        <AccordionItem value="item-4">
+                                        {/* item 3 */}
+                                        <AccordionItem value="item-3">
                                             <AccordionTrigger>Sobre</AccordionTrigger>
                                             <AccordionContent>
                                                 <a href="/sobre" className="px-4 py-2 block text-sm hover:bg-accent hover:text-accent-foreground rounded-sm">Nossa História</a>
+                                                <a href="/sobre" className="px-4 py-2 block text-sm hover:bg-accent hover:text-accent-foreground rounded-sm">Objetivo</a>
                                             </AccordionContent>
                                         </AccordionItem>
 
-                                        {/* item 5 */}
-                                        <AccordionItem value="item-5">
+                                        {/* item 4 */}
+                                        <AccordionItem value="item-4">
                                             <AccordionTrigger>Contato</AccordionTrigger>
                                             <AccordionContent>
                                                 <a href="/contato" className="px-4 py-2 block text-sm hover:bg-accent hover:text-accent-foreground rounded-sm">Contate-nos</a>
@@ -187,3 +181,183 @@ export default function Header() {
         </header>
     )
 }
+
+// <header className="w-full border-b backdrop-blur-2xl bg-gray-500/30 sticky top-0 z-50">
+// <div className="flex items-center justify-between w-full max-w-8xl mx-auto p-4">
+//   <div className="flex items-center gap-4 lg:gap-8">
+//     {/* Logo */}
+//     <div className="flex items-center lg:pl-10 lg:pr-30">
+//       <Avatar className={"w-12 h-11 lg:w-23 lg:h-17 rounded-none"}>
+//         <AvatarImage src="./logo/log1.png" />
+//         <AvatarFallback>L</AvatarFallback>
+//       </Avatar>
+//       <h2
+//         className="font-bold text-xl px-2 bg-gradient-to-r from-[#68482f] to-[#cf966a] bg-clip-text text-transparent
+//                   hover:from-[#8f5f3b] hover:to-[#e0a273]"
+//       >
+//         LogPack
+//       </h2>
+//     </div>
+
+//     {/* NavBar (Desktop) */}
+//     <NavigationMenu viewport={false} className="hidden lg:block">
+//       <NavigationMenuList className="space-x-5">
+//         <NavigationMenuItem>
+//           <NavigationMenuLink
+//             href="/"
+//             className="px-4 py-2 block text-lg font-bold rounded-md"
+//           >
+//             Home
+//           </NavigationMenuLink>
+//         </NavigationMenuItem>
+
+//         <NavigationMenuItem>
+//           <NavigationMenuTrigger className={"font-bold"}>
+//             Sobre
+//           </NavigationMenuTrigger>
+//           <NavigationMenuContent>
+//             <NavigationMenuLink
+//               href="/sobre"
+//               className="px-2 py-2 block w-27"
+//             >
+//               Nossa História
+//             </NavigationMenuLink>
+//             <NavigationMenuLink
+//               href="/sobre"
+//               className="px-2 py-2 block w-27"
+//             >
+//               Objetivo
+//             </NavigationMenuLink>
+//           </NavigationMenuContent>
+//         </NavigationMenuItem>
+
+//         <NavigationMenuItem>
+//           <NavigationMenuTrigger className={"font-bold"}>
+//             Serviços
+//           </NavigationMenuTrigger>
+//           <NavigationMenuContent>
+//             <NavigationMenuLink
+//               href="/servicos"
+//               className="px-4 py-2 block"
+//             >
+//               Assistência
+//             </NavigationMenuLink>
+//           </NavigationMenuContent>
+//         </NavigationMenuItem>
+
+//         <NavigationMenuItem>
+//           <NavigationMenuTrigger className={"font-bold"}>
+//             Contato
+//           </NavigationMenuTrigger>
+//           <NavigationMenuContent>
+//             <NavigationMenuLink
+//               href="/contato"
+//               className="px-4 py-2 block w-28"
+//             >
+//               Contate-nos
+//             </NavigationMenuLink>
+//           </NavigationMenuContent>
+//         </NavigationMenuItem>
+//       </NavigationMenuList>
+//     </NavigationMenu>
+//   </div>
+
+//   {/* Avatar do usuário e Botão de Tema (Desktop) */}
+//   <div className="hidden lg:flex items-center gap-4">
+//     <SwitchTheme />
+//     <Avatar className={"w-10 h-10 lg:w-12 lg:h-12"}>
+//       <AvatarImage src="https://github.com/shadcn.png" />
+//       <AvatarFallback>U</AvatarFallback>
+//     </Avatar>
+//   </div>
+
+//   {/* Menu Mobile */}
+//   <div className="lg:hidden flex items-center gap-4">
+//     <Drawer direction="right">
+//       <DrawerTrigger asChild>
+//         <Button variant="ghost" size="icon">
+//           <Menu className="h-6 w-6" /> {/* Ícone de 3 barrinhas */}
+//         </Button>
+//       </DrawerTrigger>
+
+//       <DrawerContent className="h-full right-0 mt-0 w-64 fixed bottom-0 rounded-none">
+//         <DrawerHeader className="text-left">
+//           <DrawerTitle>LogPack Navegação</DrawerTitle>
+//           <DrawerDescription>Explore nossas opções</DrawerDescription>
+//         </DrawerHeader>
+
+//         <div className="p-4 overflow-y-auto">
+//           <Accordion type="single" collapsible className="w-full">
+//             <AccordionItem value="item-1">
+//               <AccordionContent>
+//                 <a
+//                   href="/"
+//                   className="py-2 block text-sm hover:bg-accent hover:text-accent-foreground rounded-sm"
+//                 >
+//                   Home
+//                 </a>
+//               </AccordionContent>
+//             </AccordionItem>
+
+//             <AccordionItem value="item-2">
+//               <AccordionTrigger>Serviços</AccordionTrigger>
+//               <AccordionContent>
+//                 <a
+//                   href="/servicos"
+//                   className="px-4 py-2 block text-sm hover:bg-accent hover:text-accent-foreground rounded-sm"
+//                 >
+//                   Assistência
+//                 </a>
+//               </AccordionContent>
+//             </AccordionItem>
+
+//             <AccordionItem value="item-3">
+//               <AccordionTrigger>Sobre</AccordionTrigger>
+//               <AccordionContent>
+//                 <a
+//                   href="/sobre"
+//                   className="px-4 py-2 block text-sm hover:bg-accent hover:text-accent-foreground rounded-sm"
+//                 >
+//                   Nossa História
+//                 </a>
+//                 <a
+//                   href="/sobre"
+//                   className="px-4 py-2 block text-sm hover:bg-accent hover:text-accent-foreground rounded-sm"
+//                 >
+//                   Objetivo
+//                 </a>
+//               </AccordionContent>
+//             </AccordionItem>
+
+//             <AccordionItem value="item-4">
+//               <AccordionTrigger>Contato</AccordionTrigger>
+//               <AccordionContent>
+//                 <a
+//                   href="/contato"
+//                   className="px-4 py-2 block text-sm hover:bg-accent hover:text-accent-foreground rounded-sm"
+//                 >
+//                   Contate-nos
+//                 </a>
+//               </AccordionContent>
+//             </AccordionItem>
+//           </Accordion>
+
+//           {/* SwitchTheme e Avatar dentro da Sidebar (Mobile) */}
+//           <div className="flex items-center justify-end gap-4 mt-4 p-2 border-t">
+//             <SwitchTheme />
+//             <Avatar className={"w-10 h-10"}>
+//               <AvatarImage src="https://github.com/shadcn.png" />
+//               <AvatarFallback>U</AvatarFallback>
+//             </Avatar>
+//           </div>
+//         </div>
+//         <DrawerFooter>
+//           <DrawerClose asChild>
+//             <Button variant="outline">Fechar</Button>
+//           </DrawerClose>
+//         </DrawerFooter>
+//       </DrawerContent>
+//     </Drawer>
+//   </div>
+// </div>
+// </header>
