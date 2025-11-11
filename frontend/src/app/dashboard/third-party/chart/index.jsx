8165b3b074f9e0ff499bb6a2285ff9ebeee75fdf
ramonespier <ramon.coelhomelo@outlook.com>
2025-11-11 10:52:21 -0300

    lixo retirado apenas para integrações.

--- frontend/src/app/dashboard/third-party/chart/index.jsx
+++
@@ -1,6 +0,0 @@
-
-export { default as TotalGrowthBarChart } from './total-growth-bar-chart';
-export { default as Analytics } from './analytics';
-export { default as ReportChart } from './report-chart';
-export { default as RevenueCard } from './revenue-card';
-export { default as VisitorCard } from './visitor-card';
